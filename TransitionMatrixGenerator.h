b636c3ccb43d6d46e30c40ad972f6a896477c051
Robin <rjs02@hotmail.com>
2023-04-20 10:34:05 +0200

    code clean-up

--- TransitionMatrixGenerator.h
+++ TransitionMatrixGenerator.h
@@ -13,18 +13,56 @@
 
 using idx_t = PetscInt;
 
+double checkRowStochasticity(const Mat& A, idx_t numStates, idx_t numActions) {
+    // check if matrix is row stochastic, returns inf norm of difference between 1 and sum of row
+    // calculate row-wise sum of matrix: rowSum = A * ones
+    // rowSum should be equal to actions (since every action should sum to 1)
+    // calculate inf norm of difference between 1 and rowSum
+
+    Vec ones, rowSum, solution;
+    VecCreate(PETSC_COMM_WORLD, &ones);
+    VecSetSizes(ones, PETSC_DECIDE, numStates*numActions);
+    VecSetFromOptions(ones);
+    VecSet(ones, 1.0);
+
+    VecCreate(PETSC_COMM_WORLD, &rowSum);
+    VecSetSizes(rowSum, PETSC_DECIDE, numStates);
+    VecSetFromOptions(rowSum);
+
+    VecCreate(PETSC_COMM_WORLD, &solution);
+    VecSetSizes(solution, PETSC_DECIDE, numStates);
+    VecSetFromOptions(solution);
+    VecSet(solution, static_cast<PetscScalar>(numActions));
+
+    MatMult(A, ones, rowSum);
+    VecAXPY(solution, -1, rowSum);
+
+    double norm;
+    VecNorm(solution, NORM_INFINITY, &norm);
+
+    VecDestroy(&ones);
+    VecDestroy(&rowSum);
+    VecDestroy(&solution);
+
+    return norm;
+}
+
 void generateTransitionMatrix(Mat& A, idx_t numStates, idx_t numActions, double sparsityFactor, int seed) {
-    // matrix A is numStates x numStates*numActions, in CRS format
+    // matrix A is numStates x numStates*numActions, in CRS format TODO: CRS
     // fill matrix with sparsityFactor*numStates*numActions non-zero entries per row
     // every block j*numStates:(j+1)*numStates is a transition matrix for action j => block must sum to 1
+
+    double perturbFactor = 0.05;
+    double stdDev = 0.01;
+
 
     // random engine setup
     std::mt19937_64 gen(seed);
     std::mt19937_64 gen2(seed + 42);
     std::mt19937_64 gen3(seed + 314);
-    std::uniform_real_distribution<double> uniform_dis(0.0, 1.0);
-    std::normal_distribution<double> normal_dis(0.0, 0.01); // N(0, 0.1) to perturb sparsity for each action
-    std::uniform_int_distribution<idx_t> ind_dis(0, numStates-1);
+    std::uniform_real_distribution<double> uniform_dis(0.0, 1.0);   // to generate probabilities
+    std::normal_distribution<double> normal_dis(0.0, stdDev);      // to perturb nnz per action
+    std::uniform_int_distribution<idx_t> ind_dis(0, numStates-1);   // to generate random indices
     //const idx_t nnzPerAction = (idx_t)(sparsityFactor * numStates);
 
     // create matmpiaij matrix
@@ -36,31 +74,38 @@ void generateTransitionMatrix(Mat& A, idx_t numStates, idx_t numActions, double 
     // dense
     MatCreateSeqDense(PETSC_COMM_WORLD, numStates, numStates*numActions, PETSC_NULL, &A);
 
-
     // fill matrix
     for(idx_t rowInd = 0; rowInd < numStates; ++rowInd) {
         // for actions
-        // create vector of size nnzPerAction with random values, normalize vector
-        // generate random indices for these values in the range [i, (i+1)*numStates)
-        // insert values into matrix
+            // create vector of size nnzPerAction with random values, normalize vector
+            // generate random indices for these values in the range [i, (i+1)*numStates)
+            // insert values into matrix
 
         for(idx_t actionInd = 0; actionInd < numActions; ++actionInd) {
-            const idx_t nnzPerAction = static_cast<idx_t>((sparsityFactor + normal_dis(gen2)) * numStates);
-            printf("row: %d, action: %d, nnz: %d\n", rowInd, actionInd, nnzPerAction);
-            assert(nnzPerAction < numStates);
-            assert(nnzPerAction > 0);
+            idx_t nnzPerAction = static_cast<idx_t>((sparsityFactor + normal_dis(gen2)) * numStates);
+            //printf("row: %d, action: %d, nnz: %d\n", rowInd, actionInd, nnzPerAction);
+            if(nnzPerAction > numStates) nnzPerAction = numStates;
+            else if(nnzPerAction < 1) nnzPerAction = 1;
+
             std::vector<double> values(nnzPerAction);
             double sum = 0;
+
             // generate random values
             // TODO: perturb values for less uniformity
             for(idx_t i = 0; i < nnzPerAction; ++i) {
-                values[i] = uniform_dis(gen);
-                sum += values[i];
+                double val = uniform_dis(gen);
+                if(val < perturbFactor) {
+                    val *= 5;
+                }
+                values[i] = val;
+                sum += val;
             }
+
             // normalize values s.t. they are row-stochastic
             for(idx_t i = 0; i < nnzPerAction; ++i) {
                 values[i] /= sum;
             }
+
             // generate random indices
             std::vector<idx_t> indices(nnzPerAction, std::numeric_limits<idx_t>::max());
             idx_t ind = 0;
@@ -77,13 +122,7 @@ void generateTransitionMatrix(Mat& A, idx_t numStates, idx_t numActions, double 
             MatAssemblyBegin(A, MAT_FINAL_ASSEMBLY);
             MatAssemblyEnd(A, MAT_FINAL_ASSEMBLY);
         }
-
-
     }
-
-    // assemble matrix
-    // save to file
-
 }
 
 #endif //DISTRIBUTED_INEXACT_POLICY_ITERATION_TRANSITIONMATRIXGENERATOR_H
--- main.cpp
+++ main.cpp
@@ -29,141 +29,31 @@ int main(int argc, char** argv)
 
     Timer t;
 
-    /*
-    int rows = 200;
-    int cols = 200;
-    int seed = 8624;
-    std::mt19937 gen(seed);
-    std::uniform_real_distribution<> dis(0, 1);
-
-    auto MatValues = new PetscScalar[rows*cols];
-    std::cout << "Generating matrix values..." << std::endl;
-    for (int i = 0; i < rows*cols; i++) {
-        double rand = dis(gen);
-        if(dis(gen) < 0.3) rand = 0;
-        else if (dis(gen) > 0.8) rand *= 10;
-        MatValues[i] = rand;
-    }
-    std::cout << "Generating vector values..." << std::endl;
-    auto VecValues = new double[rows];
-    for (int i = 0; i < rows; i++) {
-        double rand = dis(gen);
-        if(rand < 0.4) rand *= 10; // perturb uniformity
-        VecValues[i] = rand;
-    }
-
-    Mat A;
-    std::cout << "Generating matrix..." << std::endl;
-    generateDenseMatrix(A, rows, cols, MatValues);
-    Vec b;
-    Vec b2;
-    std::cout << "Generating vector..." << std::endl;
-    fillVector(b, rows, VecValues);
-    VecDuplicate(b, &b2);
-    VecCopy(b, b2);
-    Vec x;
-    Vec x2;
-    VecDuplicate(b, &x);
-    VecDuplicate(b, &x2);
-
-    // Solve Ax = b using GMRES
-    // Create the linear solver context
-    std::cout << "Setup solver..." << std::endl;
-    t.start();
-    KSP ksp;
-    KSPCreate(PETSC_COMM_WORLD, &ksp);
-
-    // Set the GMRES method
-    KSPSetType(ksp, KSPGMRES);
-
-    // Set the matrix
-    KSPSetOperators(ksp, A, A);
-
-    // set tolerance
-    const double rtol = 1e-14;
-    const double atol = 0.0;
-    const unsigned long maxIter = 1000;
-    KSPSetTolerances(ksp, rtol, atol, PETSC_DEFAULT, maxIter);
-
-    // Set the right-hand side
-    KSPSetFromOptions(ksp);
-    //KSPSetConvergenceTest(ksp, my_convergence_test, nullptr, nullptr);
-    KSPSetNormType(ksp, KSP_NORM_UNPRECONDITIONED);
-    // use L2 norm (KSPMonitorTrueResidualNorm)
-
-    // Solve the system
-    std::cout << "Solve..." << std::endl;
-    KSPSolve(ksp, b, x);
-    t.stop("Solving using GMRES took: ");
-    int iterations;
-    KSPGetIterationNumber(ksp, &iterations);
-    std::cout << "Number of iterations: " << iterations << std::endl;
-
-    // solve exactly A x2 = b2 using LU factorization
-    std::cout << "Solve exactly..." << std::endl;
-    t.start();
-    MatLUFactor(A, nullptr, nullptr, nullptr);
-    MatSolve(A, b2, x2);
-    t.stop("Solving exactly took: ");
-
-    // Check the solution
-    VecAXPY(x, -1, x2);
-    PetscReal norm;
-    VecNorm(x, NORM_2, &norm);
-    std::cout << "Norm of error: " << norm << std::endl;
-
-
-    // Free the memory
-    KSPDestroy(&ksp);
-    VecDestroy(&x);
-    VecDestroy(&b);
-    MatDestroy(&A);
-    VecDestroy(&x2);
-    VecDestroy(&b2);
-    */
-
-    const unsigned long states = 500;
-    const unsigned long actions = 40;
-    const double sparsityFactor = 0.2;
+    const unsigned long states = 2000;
+    const unsigned long actions = 50;
+    const double sparsityFactor = 0.1;
     const int seed = 8624;
-
 
     // generate transition matrix
     Mat A;
+    t.start();
     generateTransitionMatrix(A, states, actions, sparsityFactor, seed);
+    t.stop("Time to generate transition matrix: ");
 
     // write transition matrix to file
-    std::string filename = "../data/P_" + std::to_string(states) + "_" + std::to_string(actions);
-    std::cout << "Writing matrix to csv:" << std::endl;
-    matrixToAscii(A, filename + ".csv");
+    std::string filename = "../data/P_" + std::to_string(states) + "_" + std::to_string(actions) + "_" + std::to_string(sparsityFactor) + "_" + std::to_string(seed);
     std::cout << "Writing matrix to bin:" << std::endl;
+    t.start();
     matrixToBin(A, filename + ".bin");
+    t.stop("Time to write matrix to file: ");
+    //std::cout << "Writing matrix to csv:" << std::endl;
+    //matrixToAscii(A, filename + ".csv");
 
-    // check row-stochasticity
     std::cout << "Checking row-stochasticity..." << std::endl;
-    Vec ones;
-    Vec rowSum;
-    Vec solution;
-    VecCreate(PETSC_COMM_WORLD, &ones);
-    VecCreate(PETSC_COMM_WORLD, &rowSum);
-    VecCreate(PETSC_COMM_WORLD, &solution);
-    VecSetSizes(ones, states*actions, PETSC_DECIDE);
-    VecSetSizes(rowSum, states, PETSC_DECIDE);
-    VecSetSizes(solution, states, PETSC_DECIDE);
-    VecSetFromOptions(ones);
-    VecSetFromOptions(rowSum);
-    VecSetFromOptions(solution);
-    VecSet(ones, 1);
-    VecSet(solution, actions);
-    // compute row-wise sum
-    MatMult(A, ones, rowSum);
-    // subtract solution, print inf norm
-    VecAXPY(rowSum, -1, solution);
-    PetscReal norm;
-    VecNorm(rowSum, NORM_INFINITY, &norm);
-    std::cout << "Row-stochasticity error: " << norm << std::endl;
-
-
+    t.start();
+    double error = checkRowStochasticity(A, states, actions);
+    std::cout << "Row-stochasticity error: " << error << std::endl;
+    t.stop("Time to check row-stochasticity: ");
 
     // destroy matrix
     MatDestroy(&A);
