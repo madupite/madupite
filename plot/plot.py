3cf63bf195c10539eb224d23fe85a683c6e3982f
Robin Sieber <rjs02@hotmail.com>
2023-06-18 11:02:12 +0200

    change script for new json layout

--- plot/plot.py
+++ plot/plot.py
@@ -5,35 +5,27 @@ import numpy as np
 # path to your json file
 json_file_path = "../cmake-build-debug/out/stats.json"
 
-# Load JSON file
 with open(json_file_path, 'r') as f:
     data = json.load(f)
 
-# Extract data
-computation_time = [d['computation_time'] for d in data['data']]
-ksp_iterations = [d['ksp_iterations'] for d in data['data']]
-pi_iterations = [d['pi_iteration'] for d in data['data']]
-residual = [d['residual'] for d in data['data']]
-
-
-# Calculate cumulative time
-cumulative_time = np.cumsum(computation_time)
-
-# Create a subplot
 fig, axs = plt.subplots(2, figsize=(10, 10))
 
-# First subplot
-axs[0].semilogy(pi_iterations, residual, 'o-')
-axs[0].set_title('Residual Norm vs Iterations')
-axs[0].set_xlabel('Iterations')
-axs[0].set_ylabel('Residual Norm (log scale)')
+for idx, solver_run in enumerate(data['solver_runs']):
+    # Compute cumulative time
+    cumulative_time = [sum([item['computation_time'] for item in solver_run[:i+1]]) for i in range(len(solver_run))]
 
-# Second subplot
-axs[1].semilogy(cumulative_time, residual, 'o-')
-axs[1].set_title('Residual Norm vs Cumulative Computation Time')
-axs[1].set_xlabel('Cumulative Computation Time')
-axs[1].set_ylabel('Residual Norm (log scale)')
+    # Plot residuals vs iterations
+    axs[0].semilogy([item['pi_iteration'] for item in solver_run], [item['residual'] for item in solver_run], label=f'Run {idx+1}')
+    axs[0].set_xlabel('Iterations')
+    axs[0].set_ylabel('Residual $||_\infty$')
+    axs[0].legend()
+
+    # Plot residuals vs cumulative time
+    axs[1].semilogy(cumulative_time, [item['residual'] for item in solver_run], label=f'Run {idx+1}')
+    axs[1].set_xlabel('Cumulative Time [ms]')
+    axs[1].set_ylabel('Residual $||_\infty$')
+    axs[1].legend()
 
 # Display plot
 plt.tight_layout()
-plt.show()
+plt.savefig('residuals.png')
