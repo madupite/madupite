70d1813733907e87eaf6513f484da7b1b8e4a5eb
Robin <rjs02@hotmail.com>
2023-06-28 13:22:00 +0200

    update euler launch scripts

--- euler/launch.sh
+++ euler/launch.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
 #SBATCH -n 16
-#SBATCH --time=02:00:00
+#SBATCH --time=01:00:00
 #SBATCH --job-name="iPI Benchmark"
 #SBATCH --mem-per-cpu=8192
 #SBATCH --mail-type=BEGIN,END
 
+module purge
 module load gcc/9.3.0
 module load cmake/3.25.0
 module load openmpi/4.1.4
 module load openblas/0.3.20
 module load petsc/3.15.5
 module load python/3.11.2
+module list
+
+lscpu
+date
 
 cd ../build
 make
 
-python ../run_benchmark.py
+# Variables
+n=9000
+m=100
+s=0.1
+
+python ../euler/run_benchmark.py -n $n -m $m -s $s
--- euler/run_benchmark.py
+++ euler/run_benchmark.py
@@ -1,33 +1,42 @@
 import os
 import subprocess
+import argparse
+
+# Define and parse command line arguments
+parser = argparse.ArgumentParser(description='Run distributed inexact policy iteration.')
+parser.add_argument('-n', '--numStates', type=int, required=True, help='Number of states.')
+parser.add_argument('-m', '--numActions', type=int, required=True, help='Number of actions.')
+parser.add_argument('-s', '--sparsityFactor', type=float, required=True, help='Sparsity factor.')
+args = parser.parse_args()
 
 # List of CPUs
 cpus = [1, 2, 4, 8, 16]
 
 # Parameters
-numStates = 9000
-numActions = 100
-sparsityFactor = 0.1
+numStates = args.numStates
+numActions = args.numActions
+sparsityFactor = args.sparsityFactor
 executable = "./distributed_inexact_policy_iteration"
 
 # Define the directory structure
 slurm_id = os.environ["SLURM_JOB_ID"]
-dir_scratch = f"/cluster/scratch/rosieber/data"
+dir_scratch = "/cluster/scratch/rosieber/data"
 dir_data = f"{numStates}_{numActions}_{sparsityFactor:.6f}"
 dir_output = f"/cluster/home/rosieber/distributed-inexact-policy-iteration/output/{slurm_id}"
 
 # Options
-flags = {
-    "-mat_type": "mpiaij",
-    "-pc_type": "none",
-    "-states": str(numStates),
-    "-actions": str(numActions),
-    "-discountFactor": "0.9",
-    "-maxIter_PI": "50",
-    "-maxIter_KSP": "10000",
-    "-rtol_KSP": "1e-4",
-    "-atol_PI": "1e-10",
-}
+flags = [
+    "-mat_type mpiaij",
+    "-pc_type none",
+    "-states", str(numStates),
+    "-actions", str(numActions),
+    "-discountFactor", str(0.9),
+    "-maxIter_PI", str(50),
+    "-maxIter_KSP", str(10000),
+    "-rtol_KSP", str(1e-4),
+    "-atol_PI", str(1e-10),
+    "-log_view"
+]
 
 # Loop through all CPUs
 for cpu in cpus:
@@ -35,20 +44,16 @@ for cpu in cpus:
     os.makedirs(dir_output+f"/{cpu}/", exist_ok=True)
 
     # Create a command with options appended
-    cmd = ["mpirun", "-n", str(cpu), executable]
+    cmd = ["mpirun", "-n", str(cpu), executable, *flags]
 
-    for option, value in flags.items():
-        cmd.extend([option, value])
+    cmd += [
+        f"-file_P", f"{dir_scratch}/{dir_data}/P.bin",
+        f"-file_g", f"{dir_scratch}/{dir_data}/g.bin",
+        f"-file_stats", f"{dir_output}/{cpu}/stats.json",
+        f"-file_policy", f"{dir_output}/{cpu}/policy.out",
+        f"-file_cost", f"{dir_output}/{cpu}/cost.out"
+    ]
     
-    cmd.extend([
-        "-file_P", f"{dir_scratch}/{dir_data}/P.bin",
-        "-file_g", f"{dir_scratch}/{dir_data}/g.bin",
-        "-file_stats", f"{dir_output}/{cpu}/stats.json",
-        "-file_policy", f"{dir_output}/{cpu}/policy.out",
-        "-file_cost", f"{dir_output}/{cpu}/cost.out",
-    ])
-    
-
     # Print the command
     print("[run_benchmark.py] Running command: ")
     print(" ".join(cmd), "\n\n")
