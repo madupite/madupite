7a22e2c22927d91ac3e980d491d1d102c19e0edb
Robin Sieber <rjs02@hotmail.com>
2024-02-08 23:10:44 +0100

    more clean up

--- example/example.py
+++ example/example.py
@@ -1,28 +1,32 @@
 import madupite as md
 import time
 
+def transprob(x, y):
+    return [[0, 1, 2], [0.1, 0.2, 0.7]]
+
+def stagecost(x, y):
+    return x * y
+
+
 with md.PETScContextManager():
     mdp = md.PyMDP()
 
+    # solve MDP from file
     mdp.setValuesFromFile("./petsc_options.txt")
-
     mdp.loadFromBinaryFile()
-
     mdp.inexactPolicyIteration()
 
-    def transprob(x, y):
-        return [[0, 1, 2], [0.1, 0.2, 0.7]]
+    start = time.time() if md.MPI_master() else 0
 
-    def stagecost(x, y):
-        return x * y
-
-    if md.MPI_master():
-        start = time.time()
+    # create MDP using functions
     mdp.createTransitionProbabilities(10000, 500, transprob)
     mdp.createStageCosts(10000, 500, stagecost)
+
+    # change inner solver and discount factor
     mdp.setOption("-ksp_type", "tfqmr")
     mdp["-discountFactor"] = "0.999"
     mdp.setValuesFromOptions()
-    if md.MPI_master():
-        print(time.time() - start)
+    end = time.time() if md.MPI_master() else 0
+
     mdp.inexactPolicyIteration()
+    print("Time to generate MDP: ", end - start)
--- main.py
+++
@@ -1,2 +0,0 @@
-import madupite as md
-
