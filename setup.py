d7681aef2c3f8c3558002a70d21b2c43d1867e8c
Robin <rjs02@hotmail.com>
2024-03-06 23:03:49 +0100

    working cmake setup + clean up

--- setup.py
+++ setup.py
@@ -1,14 +1,9 @@
 import os
+import numpy
 import subprocess
-import sys
+from Cython.Build import cythonize
 from setuptools import setup, Extension, find_packages
 from setuptools.command.build_ext import build_ext as _build_ext
-import numpy
-from Cython.Build import cythonize
-
-# needs the following to work:
-# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/robin/eth/restructured-madupite/madupite
-# must be run after installation and before running the example
 
 """
 .
@@ -33,15 +28,15 @@ from Cython.Build import cythonize
 │   │   └── MDP_setup.cpp
 │   └── utils.cpp
 """
-
-# cython code: src/madupite_wrapper.pyx
+# cython code: src/madupite.pyx
 # libmadupite.so: madupite/libmadupite.so
+
 
 class build_ext(_build_ext):
     def run(self):
         # Define the directory where this setup.py is located
         setup_dir = os.path.dirname(os.path.abspath(__file__))
-        build_dir = os.path.join(setup_dir, 'cmake-build')
+        build_dir = os.path.join(setup_dir, 'build')
 
         # Ensure the build directory exists
         if not os.path.exists(build_dir):
@@ -60,33 +55,28 @@ class build_ext(_build_ext):
         super().run()
 
 
-
-
 # Define the extension module
 extensions = [
     Extension(
-        name='madupite.madupite',  # Name of the module
-        sources=['madupite/madupite.pyx'],  # Source file
-        include_dirs=[numpy.get_include(), './include'],  # Include directories for C++ headers
+        name='madupite.madupite',
+        sources=['madupite/madupite.pyx'],
+        include_dirs=[numpy.get_include(), 'include'],
         libraries=['madupite'],  # Name of the library (without 'lib' prefix and '.so' suffix)
-        library_dirs=['./madupite'],  # Directory where the library is located
+        library_dirs=['madupite'],  # Directory where the library is located
         runtime_library_dirs=['$ORIGIN'],  # Search path for runtime libraries
         extra_compile_args=['-std=c++11'],  # Additional flags for the compiler
-        # extra_link_args=['-Wl,-rpath,$ORIGIN'],  # Additional flags for the linker
-        language='c++',  # Specify the language
+        extra_link_args=['-Wl,-rpath,$ORIGIN'],  # Additional flags for the linker (else the library is not found at runtime)
+        language='c++',
     )
 ]
+
 
 # Setup function
 setup(
     name='madupite',
-    version='5.0',
-    author='Your Name',
-    author_email='your.email@example.com',
-    description='A Python wrapper for the Madupite library',
     packages=find_packages(),
     ext_modules=cythonize(extensions, language_level="3"),
     include_package_data=True,
     zip_safe=False,
-    cmdclass={'build_ext': build_ext},  # Use custom build_ext
+    cmdclass={'build_ext': build_ext},
 )
