8304cb83026a37659e3584d855b46a48ab9bb2d7
Vaclav Hapla <vaclav.hapla@erdw.ethz.ch>
2024-03-20 16:06:07 +0100

    example.cpp minor edits

--- example/example.cpp
+++ example/example.cpp
@@ -3,11 +3,10 @@
 // Bachelor Thesis - Robin Sieber - 2023 - ETH Zürich
 //
 
-#include <petsc.h>
 #include <iostream>
+#include <random>
 #include "MDP.h"
 
-#include<random>
 
 double g(int s, int a) {
     return 1.0;
@@ -45,16 +44,17 @@ int main(int argc, char** argv)
     mdp.setValuesFromOptions();
     mdp.loadFromBinaryFile();
 
+    std::cout << "File loaded." << std::endl;
     // mdp.generateCostMatrix(g);
     // mdp.generateTransitionProbabilityTensor(P, 1, nullptr, 1, nullptr);
 
     mdp.inexactPolicyIteration();
-    std::cout << "done nr. 1" << std::endl;
+    std::cout << "Inext policy iteration #1 done." << std::endl;
     mdp.setOption("-discount_factor", "0.999"); // doesn't work anymore -> change setOptions to update member variables
     mdp.setOption("-ksp_type", "tfqmr");
     mdp.setValuesFromOptions();
     mdp.inexactPolicyIteration();
-    std::cout << "done nr. 2" << std::endl;
+    std::cout << "Inext policy iteration #2 done." << std::endl;
     mdp.~MDP();
 
     // Finalize PETSc
