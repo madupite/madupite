850bd92711690fd61254fa7e70f48f9c9c54a665
Robin Sieber <rjs02@hotmail.com>
2024-07-28 15:47:15 +0200

    compiling wrapper

--- src/pymadupite.cpp
+++ src/pymadupite.cpp
@@ -7,20 +7,60 @@
 #include <nanobind/stl/pair.h>
 #include <nanobind/stl/string.h>
 #include <nanobind/stl/vector.h>
+#include <nanobind/stl/shared_ptr.h>
 
 namespace nb = nanobind;
+using namespace nb::literals;
 
 NB_MODULE(_madupite_impl, m)
 {
-    m.doc() = "Python bindings for madupite";
+    // Madupite
+    nb::class_<Madupite>(m, "Madupite");
+    m.def("initialize_madupite", []() { return Madupite::initialize(nullptr, nullptr); }, "Initialize Madupite instance");
+    // nb::class_<MPI_Comm>(m, "MPI_Comm");
+    m.def("COMM_WORLD", []() { return PETSC_COMM_WORLD; }, "Global communicator for MPI");
 
-    // nb::class_<Madupite>(m, "Madupite").def("initialize", []() { return Madupite::initialize(nullptr, nullptr); });
 
-    // nb::class_<MatrixPreallocation>(m, "MatrixPreallocation")
-    //     .def_rw("d_nz", &MatrixPreallocation::d_nz)
-    //     .def_rw("d_nnz", &MatrixPreallocation::d_nnz)
-    //     .def_rw("o_nz", &MatrixPreallocation::o_nz)
-    //     .def_rw("o_nnz", &MatrixPreallocation::o_nnz);
+    //////////
+    // Matrix
+    //////////
+    nb::enum_<MatrixType>(m, "MatrixType")
+        .value("Dense", MatrixType::Dense)
+        .value("Sparse", MatrixType::Sparse)
+        .export_values();
 
-    m.def("add", [](int a, int b) { return a + b; }, "Add two integers");
+    nb::enum_<MatrixCategory>(m, "MatrixCategory")
+        .value("Dynamics", MatrixCategory::Dynamics)
+        .value("Cost", MatrixCategory::Cost)
+        .export_values();
+
+    nb::class_<MatrixPreallocation>(m, "MatrixPreallocation")
+        .def(nb::init<>())
+        .def_rw("d_nz", &MatrixPreallocation::d_nz)
+        .def_rw("d_nnz", &MatrixPreallocation::d_nnz)
+        .def_rw("o_nz", &MatrixPreallocation::o_nz)
+        .def_rw("o_nnz", &MatrixPreallocation::o_nnz);
+
+    nb::class_<Matrix>(m, "Matrix")
+        .def(nb::init<>())
+        .def_static("typeToString", &Matrix::typeToString)
+        .def_static("fromFile", &Matrix::fromFile)
+        .def("writeToFile", &Matrix::writeToFile);
+
+    m.def("createTransitionProbabilityTensor", &createTransitionProbabilityTensor);
+    m.def("createStageCostMatrix", &createStageCostMatrix);
+
+    //////////
+    // MDP 
+    //////////
+    nb::class_<MDP>(m, "MDP")
+        .def(nb::init<std::shared_ptr<Madupite>, MPI_Comm>(), 
+             nb::arg("madupite"), nb::arg("comm") = PETSC_COMM_WORLD)
+        .def("setOption", &MDP::setOption,
+             nb::arg("option"), nb::arg("value") = nullptr)
+        .def("clearOptions", &MDP::clearOptions)
+        .def("setStageCostMatrix", &MDP::setStageCostMatrix)
+        .def("setTransitionProbabilityTensor", &MDP::setTransitionProbabilityTensor)
+        .def("setUp", &MDP::setUp)
+        .def("solve", &MDP::solve);
 }
