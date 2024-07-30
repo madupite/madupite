52c6fadfb27fd1b98c4b37f484de2575aba65076
Robin Sieber <rjs02@hotmail.com>
2024-07-30 13:58:56 +0200

    finally working

--- examples/ci_test/ci_test.py
+++ examples/ci_test/ci_test.py
@@ -1,9 +1,9 @@
 import madupite
 
-def r(s, a):
+def rewardfunc(s, a):
     return 1.0 if s == 41 else 0.0
 
-def P(s, a):
+def probfunc(s, a):
     if a == 0:  # stay
         values = [0.9, 0.05, 0.05]
         indices = [s, (s - 1 + 50) % 50, (s + 1) % 50]
@@ -49,16 +49,16 @@ def main():
     mdp.setOption("-file_policy", "ci_policy.out")
     mdp.setOption("-ksp_type", "gmres")
 
-    # num_states = 50
-    # num_actions = 3
-    # prealloc = madupite.MatrixPreallocation()
-    # prealloc.o_nz = 3
-    # prealloc.d_nz = 3
-    # g = madupite.createStageCostMatrix(name="g", numStates=num_states, numActions=num_actions, func=r)
-    # P = madupite.createTransitionProbabilityTensor(name="P", numStates=num_states, numActions=num_actions, func=P, preallocation=prealloc)
+    num_states = 50
+    num_actions = 3
+    prealloc = madupite.MatrixPreallocation()
+    prealloc.o_nz = 3
+    prealloc.d_nz = 3
+    g = madupite.createStageCostMatrix(name="g", numStates=num_states, numActions=num_actions, func=rewardfunc)
+    P = madupite.createTransitionProbabilityTensor(name="P", numStates=num_states, numActions=num_actions, func=probfunc, preallocation=prealloc)
 
-    g = madupite.Matrix.fromFile(comm=madupite.getCommWorld(), name="g", filename="100_50_0.1/g.bin", category=madupite.MatrixCategory.Cost, type=madupite.MatrixType.Dense)
-    P = madupite.Matrix.fromFile(comm=madupite.getCommWorld(), name="P", filename="100_50_0.1/P.bin", category=madupite.MatrixCategory.Dynamics, type=madupite.MatrixType.Sparse)
+    # g = madupite.Matrix.fromFile(comm=madupite.getCommWorld(), name="g", filename="100_50_0.1/g.bin", category=madupite.MatrixCategory.Cost, type=madupite.MatrixType.Dense)
+    # P = madupite.Matrix.fromFile(comm=madupite.getCommWorld(), name="P", filename="100_50_0.1/P.bin", category=madupite.MatrixCategory.Dynamics, type=madupite.MatrixType.Sparse)
 
     mdp.setStageCostMatrix(g)
     mdp.setTransitionProbabilityTensor(P)
--- src/MDP/MDP_matrix.cpp
+++ src/MDP/MDP_matrix.cpp
@@ -1,13 +1,14 @@
 #include "MDP_matrix.h"
+#include "MDP.h"
 
 Matrix createTransitionProbabilityTensor(
     MPI_Comm comm, const std::string& name, PetscInt numStates, PetscInt numActions, Probfunc func, const MatrixPreallocation& pa)
 {
     PetscInt localStates = PETSC_DECIDE;
-    PetscSplitOwnership(comm, &localStates, &numStates);
-    Layout rowLayout(comm, localStates * numActions, true);
-    Layout colLayout(comm, localStates, true);
-    Matrix A(comm, name, MatrixType::Sparse, rowLayout, colLayout, pa);
+    PetscSplitOwnership(Madupite::getCommWorld(), &localStates, &numStates);
+    Layout rowLayout(Madupite::getCommWorld(), localStates * numActions, true);
+    Layout colLayout(Madupite::getCommWorld(), localStates, true);
+    Matrix A(Madupite::getCommWorld(), name, MatrixType::Sparse, rowLayout, colLayout, pa);
     auto   lo = rowLayout.start();
     auto   hi = rowLayout.end();
 
@@ -27,12 +28,12 @@ Matrix createTransitionProbabilityTensor(
 Matrix createStageCostMatrix(MPI_Comm comm, const std::string& name, PetscInt numStates, PetscInt numActions, Costfunc func)
 {
     PetscInt localStates = PETSC_DECIDE;
-    PetscSplitOwnership(comm, &localStates, &numStates);
-    Layout   rowLayout(comm, localStates, true);
+    PetscSplitOwnership(Madupite::getCommWorld(), &localStates, &numStates);
+    Layout   rowLayout(Madupite::getCommWorld(), localStates, true);
     PetscInt localActions = PETSC_DECIDE;
-    PetscSplitOwnership(comm, &localActions, &numActions);
-    Layout colLayout(comm, localActions, true);
-    Matrix A(comm, name, MatrixType::Dense, rowLayout, colLayout);
+    PetscSplitOwnership(Madupite::getCommWorld(), &localActions, &numActions);
+    Layout colLayout(Madupite::getCommWorld(), localActions, true);
+    Matrix A(Madupite::getCommWorld(), name, MatrixType::Dense, rowLayout, colLayout);
     auto   g_start_ = rowLayout.start();
     auto   g_end_   = rowLayout.end();
 
