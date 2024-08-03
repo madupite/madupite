08db6b4f1fa5714bf69871f4684660ffad1359a5
Philip Pawlowsky <philip.pawlowsky@gmail.com>
2024-08-03 11:46:58 +0200

    stub generation

---
+++ src/madupite/__init__.pyi
@@ -0,0 +1,14 @@
+from . import madupite as madupite
+from .madupite import (
+    MDP as MDP,
+    Madupite as Madupite,
+    Matrix as Matrix,
+    MatrixCategory as MatrixCategory,
+    MatrixPreallocation as MatrixPreallocation,
+    MatrixType as MatrixType,
+    createStageCostMatrix as createStageCostMatrix,
+    createTransitionProbabilityTensor as createTransitionProbabilityTensor,
+    getCommWorld as getCommWorld,
+    initialize_madupite as initialize_madupite,
+    mpi_rank_size as mpi_rank_size
+)
