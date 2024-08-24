9c1a8f7e5f2c47fc3ec6833a593f22a7fe2ad57f
Philip Pawlowsky <ppawlowsky@ethz.ch>
2024-08-24 12:34:59 +0200

    launch file for pendulum

---
+++ examples/pendulum/euler-launch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --output=./data/%j.out
+#SBATCH --error=./data/%j.err
+
+#SBATCH --time=0:01:00
+#SBATCH --ntasks=6
+#SBATCH --mem-per-cpu=20G
+
+mpirun python compute_pendulum.py;
+# Make sure to call sbatch from <...>/madupite/examples/pendulum/ to have the correct relative filenames
+
