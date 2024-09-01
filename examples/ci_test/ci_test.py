fd3fa770fe7272402351bb3c84f6889dd157bed8
gmatilde <gmatilde@itet-ifa-601.ethz.ch>
2024-09-01 12:03:15 +0200

    minor

--- examples/ci_test/ci_test.py
+++ examples/ci_test/ci_test.py
@@ -56,7 +56,7 @@ def main():
     mdp.setTransitionProbabilityTensor(P)
     mdp.solve()
 
-    #We now update the mdp parameters by loading them from .bin files which are stored in the ./data folder. We can re-use the same mdp instance, but we simply update the stage cost matrix and the transition probability tensor with the values loaded from files.
+    #We now update the mdp parameters by loading them from .bin files which are stored in the ./data folder. We can re-use the same mdp instance by updating the stage cost matrix and the transition probability tensor with the values loaded from files.
     mdp.setOption("-file_stats", "ci_stats_fromfile.json")
     mdp.setOption("-file_cost", "ci_reward_fromfile.out")
     mdp.setOption("-file_policy", "ci_policy_fromfile.out")
